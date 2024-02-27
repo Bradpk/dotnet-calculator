f7fd2db3da424b6914f160678e2ca203cfcceaa1
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-27 15:46:53 -0500

    g

--- obj/Debug/net8.0/dotnet-calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e7f82603ce9c24f220f1a4d9e9a416508d648f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78485a28f5c7bcd3bd5af54681d227006e98bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
