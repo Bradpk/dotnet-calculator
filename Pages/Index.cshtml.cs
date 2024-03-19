88afdc7f7fe4688d78a7cda5415cdedaf15ef894
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-03-19 14:02:23 -0400

    g

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,37 +13,41 @@ namespace dotnet_calculator.Pages
         {
         }
 
-        public void OnPost(string button)
-        {
-            switch (button)
-            {
-                case "CE":
-                    Screen = "0";
-                    break;
-                case "=":
-                    try
-                    {
-                        Screen = Evaluate(Screen).ToString();
-                    }
-                    catch
-                    {
-                        Screen = "Error";
-                    }
-                    break;
-                default:
-                    if (Screen == "0")
-                    {
-                        Screen = button;
-                    }
-                    else
-                    {
-                        Screen += button;
-                    }
-                    break;
-            }
-        }
+public void OnPost(string button)
+{
+    Screen = TempData["Screen"] as string ?? "0";
 
-        private double Evaluate(string expression)
+    switch (button)
+    {
+        case "CE":
+            Screen = "0";
+            break;
+        case "=":
+            try
+            {
+                Screen = Evaluate(Screen).ToString();
+            }
+            catch
+            {
+                Screen = "Error";
+            }
+            break;
+        default:
+            if (Screen == "0" && button != "0")
+            {
+                Screen = button;
+            }
+            else
+            {
+                Screen += button;
+            }
+            break;
+    }
+
+    TempData["Screen"] = Screen;
+}
+
+        private static double Evaluate(string expression)
         {
             var dataTable = new DataTable();
             return (double)Convert.ChangeType(dataTable.Compute(expression, null), typeof(double));
--- obj/Debug/net8.0/dotnet-calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78485a28f5c7bcd3bd5af54681d227006e98bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fd2db3da424b6914f160678e2ca203cfcceaa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
